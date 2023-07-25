00931b1fdbf3427c7f857457acd4a95fe0947b07
Priyam Mondal <priyammondal096@gmail.com>
2023-07-25 08:50:05 +0530

    push

--- tutorial-4/index.js
+++ tutorial-4/index.js
@@ -1,1 +1,57 @@
-console.log("this is tutorial-4")
+console.log("this is tutorial-4");
+
+// let num1 = 5;
+// let num2 = 2;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// console.log(num1 ** num2);
+
+// console.log(4 ** (1 / 2));
+// console.log("6" / "3");
+// console.log(8 - "4");
+
+// let num3 = "6";
+// console.log(typeof num3);
+// let num4 = +num3;
+// console.log(typeof num4);
+
+// let num5 = undefined;
+// let num6 = +undefined;
+// console.log(num6);
+
+// let num7 = 4;
+// let num8 = 9;
+
+// num8--;
+// num7++;
+// console.log(num8); //8
+// console.log(num7); //5
+
+// num7 = num7 + --num8;
+// console.log(num7); //12
+
+// let num8 = (4 - 3, 8 * 9); //72
+
+// console.log("something ", num8);
+
+// <, > , <=, >=, ==, !=, ===,
+
+// let num1 = 9;
+// let num2 = 6;
+
+// console.log(num1 > num2);
+// console.log(num1 < num2);
+// console.log(num1 == num2);
+// console.log(num1 >= num2);
+// console.log(num1 <= num2);
+// console.log(num1 === num2);
+// console.log(num1 != num2);
+// console.log(num1 !== num2);
+
+console.log("A" > "Z");
+console.log("a" > "Z");
+console.log("a" > "z");
