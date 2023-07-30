1db64f6d8ce21144c469b34dccc2258932748b37
Priyam Mondal <priyammondal096@gmail.com>
2023-07-30 15:03:47 +0530

    tutorial-5

---
+++ tutorial-5/index.js
@@ -0,0 +1,42 @@
+// conditional branching
+
+// let age = prompt("Pleae Enter Your Age:");
+
+// if (age > 18) {
+//   console.log("You are eligible for driving a car");
+// } else if (age == 18) {
+//   console.log("You are eligible for driving a bike but not a car!");
+// } else {
+//   console.log("Sorry, You are not eligible for driving");
+// }
+
+// let age = prompt("Please enter your age: ");
+
+// let result =
+//   age > 18
+//     ? "You are eligible for driving"
+//     : age === "18"
+//     ? "You are eligible for driving a bike but not a car!"
+//     : "Sorry, you are not eligible for driving";
+
+// console.log(result);
+
+// Logical Operators
+// 1. Logical And (&&)
+// let userName = "Sourav";
+// let age = 22;
+
+// if (userName == "Ramesh" || age == 22) {
+//   console.log("something");
+// } else {
+//   console.log("this is false");
+// }
+
+// console.log(undefined || 0 || null);
+
+// let age = NaN;
+
+// let result = age ?? "This is else part";
+// console.log(result);
+
+
